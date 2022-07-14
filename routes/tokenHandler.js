f152c71854b71a364e15c7fc4f9152d6798e1619
hasibulislam999 <hasib143sl@gmail.com>
2022-07-14 19:17:35 +0600

    post method on token complete

---
+++ routes/tokenHandler.js
@@ -0,0 +1,100 @@
+/**
+ * Title: Token handler route
+ * Description: Handler that handle token to make secure RESTful APIs
+ * Author: Hasibul Islam
+ * Date: 14/07/2022
+ */
+
+/* dependency */
+const data = require('../lib/data');
+const { hash } = require('../utilities/hashUtils');
+const { parseJSON } = require('../utilities/userUtils');
+const { createRandomStrings } = require('../utilities/tokenUtils');
+
+/* handle user object - module scaffolding */
+const handle = {};
+
+/* creating sample handler function within handle object */
+handle.tokenHandler = (requestProperties, callBack) => {
+    // approaching server methods
+    const acceptedMethod = ['get', 'post', 'put', 'delete'];
+
+    // validate getting method
+    if (acceptedMethod.indexOf(requestProperties.method) > -1) {
+        // catch exact method client side hit to server
+        handle._token[requestProperties.method](requestProperties, callBack);
+    } else {
+        // method not allowed
+        callBack(405)
+    }
+}
+
+/* user secret object - module scaffolding */
+handle._token = {};
+
+/* let fetch with methods */
+handle._token.post = (requestProperties, callBack) => {
+    // approaching phone number
+    const phoneNumber = typeof requestProperties.userInfo.phoneNumber === 'string'
+        && requestProperties.userInfo.phoneNumber.trim().length === 11
+        ? requestProperties.userInfo.phoneNumber
+        : null;
+
+    // approaching password
+    const password = typeof requestProperties.userInfo.password === 'string'
+        && requestProperties.userInfo.password.trim().length > 0
+        ? requestProperties.userInfo.password
+        : null;
+
+    // validate with phone number & password
+    if (password && phoneNumber) {
+        data.read('users', phoneNumber, (error, usr) => {
+            // parse data from json format
+            const userData = { ...parseJSON(usr) };
+            // convert password to hashed password
+            const hashedPassword = hash(password);
+            if (hashedPassword === userData.password) {
+                // token credentials
+                const tokenID = createRandomStrings(16);
+                const tokenExpires = Date.now() + (60 * 60 * 1000);
+                // stored in a object
+                const tokenObject = {
+                    id: tokenID,
+                    phoneNumber,
+                    expiry: tokenExpires
+                };
+
+                // store the token in db
+                data.create('tokens', tokenID, tokenObject, (err) => {
+                    if (!err) {
+                        callBack(200, tokenObject);
+                    } else {
+                        callBack(500, {
+                            message: 'error occurs in server side'
+                        })
+                    }
+                })
+            } else {
+                callBack(401, {
+                    message: 'invalid credential request'
+                })
+            }
+        })
+    } else {
+        callBack(400, {
+            message: 'invalid request'
+        })
+    }
+};
+
+/* @TODO: authentication required */
+handle._token.get = (requestProperties, callBack) => { };
+
+/* @TODO: authentication required */
+handle._token.put = (requestProperties, callBack) => { };
+
+/* @TODO: authentication required */
+handle._token.delete = (requestProperties, callBack) => { };
+
+/* export module as external module */
+module.exports = handle;
