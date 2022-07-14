ecaa4697dc385328f8d48e49b44d526af52a0972
hasibulislam999 <hasib143sl@gmail.com>
2022-07-14 12:43:50 +0600

    file strorage or manual db file adding testing added

--- index.js
+++ index.js
@@ -9,9 +9,20 @@
 const http = require('http');
 const { handleReqRes } = require('./utilities/handleReqRes');
 const environment = require('./utilities/environment');
+const data = require('./lib/data');
 
 /* app object - module scaffolding */
 const app = {};
+
+/* @testing: removed later */
+// data.create('', 'newFile', {
+//     name: "Hasibul Islam",
+//     id: 1935202062,
+//     dept: "CSE",
+//     institute: 'City University'
+// }, err => {
+//     process.stdout.write(`error is ${err}`);
+// })
 
 /* create server */
 app.createServer = () => {
