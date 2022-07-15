3ab141e36afdb86e7bdb4c3169d3aabf996db674
hasibulislam999 <hasib143sl@gmail.com>
2022-07-15 14:53:25 +0600

    example deleted on index file

--- index.js
+++ index.js
@@ -9,39 +9,9 @@
 const http = require('http');
 const { handleReqRes } = require('./utilities/handleReqRes');
 const environment = require('./utilities/environment');
-const data = require('./lib/data');
 
 /* app object - module scaffolding */
 const app = {};
-
-/* @testing: removed later */
-// !# able to write a file
-// data.create('', 'test', {
-//     name: "Hasibul Islam",
-//     id: 1935202062,
-//     dept: "CSE",
-// }, err => {
-//     console.log(err);
-// })
-
-// !# able to read a file
-// data.read('', 'test', (error, data) => {
-//     console.log(error, data);
-// })
-
-// !# able to update a file
-// data.update('', 'test', {
-//     institute: 'City University',
-//     established: 2010,
-//     location: 'Ashulia, Khagan, Dhaka'
-// }, err => {
-//     console.log(err);
-// });
-
-// !# able to delete an existing file
-// data.delete('', 'test', err => {
-//     console.log(err);
-// })
 
 /* create server */
 app.createServer = () => {
